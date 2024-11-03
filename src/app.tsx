4186e6159c63fb86029699c644025fafcc2e9654
Moses Dumbuya <moses2004112@hotmail.com>
2024-11-03 01:07:17 +0100

    Database setup

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,8 @@ import { federation } from "@fedify/fedify/x/hono";
 import { getLogger } from "@logtape/logtape";
 import fedi from "./federation.ts";
 import { Layout, SetupForm } from "./views.tsx";
+import db from "./db.ts";
+import type { User } from "./schema.ts";
 
 const logger = getLogger("prototypeMicroblogAP");
 
@@ -18,5 +20,31 @@ app.get("/setup", (c) =>
     </Layout>,
   ),
 );
+
+app.post("/setup", async (c) => {
+  // Check if an account already exists
+  const user = db.prepare<unknown[], User>("SELECT * FROM users LIMIT 1").get();
+  if (user != null) return c.redirect("/");
+
+  const form = await c.req.formData();
+  const username = form.get("username");
+  if (typeof username !== "string" || !username.match(/^[a-z0-9_-]{1,50}$/)) {
+    return c.redirect("/setup");
+  }
+  db.prepare("INSERT INTO users (username) VALUES (?)").run(username);
+  return c.redirect("/");
+});
+
+app.get("/setup", (c) => {
+  // Check if an account already exists
+  const user = db.prepare<unknown[], User>("SELECT * FROM users LIMIT 1").get();
+  if (user != null) return c.redirect("/");
+
+  return c.html(
+    <Layout>
+      <SetupForm />
+    </Layout>,
+  );
+});
 
 export default app;
