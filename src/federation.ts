49948dddfec14af2649468928c4ed5988f3901c4
Moses Dumbuya <moses2004112@hotmail.com>
2024-11-05 14:53:07 +0100

    Implementation of Crypto Keys

--- src/federation.ts
+++ src/federation.ts
@@ -1,8 +1,7 @@
-import { Endpoints, Person, createFederation } from "@fedify/fedify";
+import { Endpoints, Person, createFederation, MemoryKvStore, InProcessMessageQueue, exportJwk, generateCryptoKeyPair, importJwk } from "@fedify/fedify";
 import { getLogger } from "@logtape/logtape";
-import { MemoryKvStore, InProcessMessageQueue } from "@fedify/fedify";
 import db from "./db.ts";
-import type { Actor, User } from "./schema.ts";
+import type { Actor, User, Key } from "./schema.ts";
 
 const logger = getLogger("prototypeMicroblogAP");
 
@@ -11,29 +10,82 @@ const federation = createFederation({
   queue: new InProcessMessageQueue(),
 });
 
-federation.setActorDispatcher("/users/{identifier}", async (ctx, identifier) => {
-  const user = db
-    .prepare<unknown[], User & Actor>(
-      `
-      SELECT * FROM users
-      JOIN actors ON (users.id = actors.user_id)
-      WHERE users.username = ?
-      `,
-    )
-    .get(identifier);
-  if (user == null) return null;
+federation
+  .setActorDispatcher("/users/{identifier}", async (ctx, identifier) => {
+    const user = db
+      .prepare<unknown[], User & Actor>(
+        `
+        SELECT * FROM users
+        JOIN actors ON (users.id = actors.user_id)
+        WHERE users.username = ?
+        `,
+      )
+      .get(identifier);
+    if (user == null) return null;
 
-  return new Person({
-    id: ctx.getActorUri(identifier),
-    preferredUsername: identifier,
-    name: user.name,
-    inbox: ctx.getInboxUri(identifier),
-    endpoints: new Endpoints({
-      sharedInbox: ctx.getInboxUri(),
-    }),
-    url: ctx.getActorUri(identifier),
+    const keys = await ctx.getActorKeyPairs(identifier);
+    return new Person({
+      id: ctx.getActorUri(identifier),
+      preferredUsername: identifier,
+      name: user.name,
+      inbox: ctx.getInboxUri(identifier),
+      endpoints: new Endpoints({
+        sharedInbox: ctx.getInboxUri(),
+      }),
+      url: ctx.getActorUri(identifier),
+      publicKey: keys[0].cryptographicKey,
+      assertionMethods: keys.map((k) => k.multikey),
+    });
+  })
+  .setKeyPairsDispatcher(async (ctx, identifier) => {
+    const user = db
+      .prepare<unknown[], User>("SELECT * FROM users WHERE username = ?")
+      .get(identifier);
+    if (user == null) return [];
+    const rows = db
+      .prepare<unknown[], Key>("SELECT * FROM keys WHERE keys.user_id = ?")
+      .all(user.id);
+    const keys = Object.fromEntries(
+      rows.map((row) => [row.type, row]),
+    ) as Record<Key["type"], Key>;
+    const pairs: CryptoKeyPair[] = [];
+    // For each of the two key formats (RSASSA-PKCS1-v1_5 and Ed25519) that
+    // the actor supports, check if they have a key pair, and if not,
+    // generate one and store it in the database:
+    for (const keyType of ["RSASSA-PKCS1-v1_5", "Ed25519"] as const) {
+      if (keys[keyType] == null) {
+        logger.debug(
+          "The user {identifier} does not have an {keyType} key; creating one...",
+          { identifier, keyType },
+        );
+        const { privateKey, publicKey } = await generateCryptoKeyPair(keyType);
+        db.prepare(
+          `
+          INSERT INTO keys (user_id, type, private_key, public_key)
+          VALUES (?, ?, ?, ?)
+          `,
+        ).run(
+          user.id,
+          keyType,
+          JSON.stringify(await exportJwk(privateKey)),
+          JSON.stringify(await exportJwk(publicKey)),
+        );
+        pairs.push({ privateKey, publicKey });
+      } else {
+        pairs.push({
+          privateKey: await importJwk(
+            JSON.parse(keys[keyType].private_key),
+            "private",
+          ),
+          publicKey: await importJwk(
+            JSON.parse(keys[keyType].public_key),
+            "public",
+          ),
+        });
+      }
+    }
+    return pairs;
   });
-});
 
 federation.setInboxListeners("/users/{identifier}/inbox", "/inbox");
 
